82239b42a6b775f0acd2fd6601776fee3d049456
MeesterMarcus <lorenzanamarcus@gmail.com>
2025-06-15 20:54:37 -0500

    changes

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,15 @@
 module.exports = function(eleventyConfig) {
   // Set input and output directories
   eleventyConfig.addPassthroughCopy("assets");
+
+  // Explicitly set Nunjucks as the template engine for HTML files
+  eleventyConfig.htmlTemplateEngine = "njk";
+
   return {
     dir: {
       input: ".",
-      output: "_site"
+      output: "_site",
+      includes: "_includes"
     }
   };
 };
